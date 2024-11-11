1312e76f201b1b6e7a5c7908b1afe7da4a826ce2
Jason Warren <jason.warren@vuluru.com>
2024-11-11 02:16:47 -0500

    Merge remote-tracking branch 'origin/November2024beta234' into November2024beta234
    # Conflicts:
    #	public/stylesheets/style.css
    #	views/register.pug

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,9 @@
-// routes/dashboard.js
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated } = require('../middleware/authMiddleware');
 
-router.get('/dashboard', ensureAuthenticated, (req, res) => {
-    res.render('dashboard', { user: req.user });
+// GET /dashboard - Render the main dashboard
+router.get('/', (req, res) => {
+    res.render('dashboard', { title: 'Dashboard' });
 });
 
 module.exports = router;
